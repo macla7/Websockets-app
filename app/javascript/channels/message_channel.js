967058c2be100d2911bd74906e7ad22c4b3ce9cd
Mitch <MitchelClark1997@outlook.com>
2021-04-24 08:46:28 +1000

    init commit

---
+++ app/javascript/channels/message_channel.js
@@ -0,0 +1,29 @@
+import consumer from "./consumer"
+
+const messageChannel = consumer.subscriptions.create("MessageChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    console.log(data)
+    console.log('hi')
+    const messageDisplay = document.querySelector('#message-display')
+    messageDisplay.insertAdjacentHTML('beforeend', this.template(data))
+  },
+
+  template(data) {
+    return `<article class="message">
+              <div class="message-header">
+                <p>${data.user.email}</p>
+              </div>
+              <div class="message-body">
+                <p>${data.body}</p>
+              </div>
+            </article>`
+  }
+});
